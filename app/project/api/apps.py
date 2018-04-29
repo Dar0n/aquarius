c40127baac5daf71d7d3e3fa51a65509efa0a229
Ilya <shatuncheg@gmail.com>
2018-04-29 17:52:40 +0200

    fixed flake8 errors

--- app/project/api/apps.py
+++ app/project/api/apps.py
@@ -2,6 +2,7 @@ from django.apps import AppConfig
 from django.contrib.auth import get_user_model
 from django.db.models.signals import post_save
 
+
 # This part here is to use signal defined in signals.py file to automatically create user profile when creating a user
 class MyAppConfig(AppConfig):
     name = 'project.api'
