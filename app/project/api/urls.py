e191aca7905429773f1e8760db6732890f0aabae
Ilya <shatuncheg@gmail.com>
2018-04-30 18:55:55 +0200

    Add urls, views and serializers for reviews API endpoints. Change expiration period for tokens to 5 days. Add urls, views and serializers for categories API endpoints. Add IsOwnerOrReadOnly permission.

--- app/project/api/urls.py
+++ app/project/api/urls.py
@@ -1,15 +1,19 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+
+from project.api.views.categories import GetCategoriesView
 from project.api.views.registration import RegistrationView, RegistrationValidationView
 #
 from project.api.views.password_reset import PasswordResetView, PasswordResetValidationView
+from project.api.views.reviews import RestaurantReviewsView, ReviewCreateView, ReviewByUserView, \
+    GetPostDeleteReviewView, ReviewLikeDislikeView, LikedReviewsView, CommentedReviewsView
+
 # from project.api.views.feed import FeedDisplayView, UserGetAllPosts, PostFromFollowingsView, ListFriendsPostsView
 # from project.api.views.posts import PostGetUpdateDeleteView, PostCreateView, PostThatUserLiked, PostLikeDislikeView, \
 #     ShareAPostView
 # from project.api.views.users import ProfileFollowUnfollowAUser, ProfileFollowingUsersView, ProfileFollowerUsersView, \
 #     AllUsersView, GetUserProfile, UserFriendRequests, SendFriendRequestView, PendingFriendRequestsView, \
 #     AcceptFriendRequestView, RejectFriendRequestView, ListAllFriendsView, UnfriendAFriendsView, UserProfileView
-
 
 app_name = "api"
 
@@ -21,17 +25,14 @@ urlpatterns = [
     path("registration/validation/", RegistrationValidationView.as_view(), name="registration-validation"),
     path("auth/password-reset/", PasswordResetView.as_view(), name="password-reset"),
     path("auth/password-reset/validate/", PasswordResetValidationView.as_view(), name="password-reset-validation"),
-    # path("feed/", FeedDisplayView.as_view(), name="feed_display"),
-    # path("feed/<int:user_id>/", UserGetAllPosts.as_view(), name="user-all-posts"),
-    # path("feed/followings/", PostFromFollowingsView.as_view(), name="all-posts-from-followers"),
-    # path("feed/friends/", ListFriendsPostsView.as_view(), name="list-friends"),
-    # path("posts/<int:post_id>/", PostGetUpdateDeleteView.as_view(), name="post_detail"),
-    # path("posts/new-post/", PostCreateView.as_view(), name="post-create"),
-    # path("posts/likes/", PostThatUserLiked.as_view(), name="liked-posts"),
-    # path("posts/like/<int:post_id>/", PostLikeDislikeView.as_view(), name="like-dislike-a-post"),
-    # path("posts/share-post/<int:post_id>/", ShareAPostView.as_view(), name="share-a-post"),
-    # path("users/follow/<int:user_id>/", ProfileFollowUnfollowAUser.as_view(), name="follow-a-user"),
-    # path("<int:user_id>/following/", ProfileFollowingUsersView.as_view(), name="following-users"),
+    path("reviews/restaurant/<int:restaurant_id>/", RestaurantReviewsView.as_view(), name="restaurant-reviews"),
+    path("reviews/new_review/<int:pk>/", ReviewCreateView.as_view(), name="create-review"),
+    path("reviews/user/<int:user_id>/", ReviewByUserView.as_view(), name="show-reviews-for-user"),
+    path("reviews/<int:review_id>/", GetPostDeleteReviewView.as_view(), name="get-post-delete-review"),
+    path("reviews/like/<int:review_id>/", ReviewLikeDislikeView.as_view(), name="like-dislike-review"),
+    path("reviews/likes/", LikedReviewsView.as_view(), name="liked-reviews"),
+    path("reviews/comments/", CommentedReviewsView.as_view(), name="commented-reviews"),
+    path("category/list/", GetCategoriesView.as_view(), name='get-list-of-categories'),
     # path("<int:user_id>/followers/", ProfileFollowerUsersView.as_view(), name="followers"),
     # path("users/", AllUsersView.as_view(), name="list-all-users"),
     # path("users/<int:user_id>/", GetUserProfile.as_view(), name="get-user-profile"),
