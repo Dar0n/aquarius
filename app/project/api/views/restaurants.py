3d70edd8c13917ddf27df94dec93d49e98286860
Sirinya <30613818+halfpintutopia@users.noreply.github.com>
2018-04-30 14:11:36 +0200

    -user and restaurant models have been updated. Restaurant serializer has been added. User serializer has been edited. Last linting issues resolved

--- app/project/api/views/restaurants.py
+++ app/project/api/views/restaurants.py
@@ -2,7 +2,6 @@ from django.contrib.auth.models import User
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
-from rest_framework import serializers
 
 
 # @route   POST api/restaurants/new/
