fdd725cb7d9fe2b62ed92c97f4f5a60fb6cd07f1
Ilya <shatuncheg@gmail.com>
2018-04-30 11:58:24 +0200

    Add url to registration serializer, modify url pattern - add /backend/ before api/ and admin/ so to separate our future front-end from back-end.

--- app/project/api/serializers/registration.py
+++ app/project/api/serializers/registration.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
 from rest_framework import serializers
 
 User = get_user_model()
@@ -24,8 +25,8 @@ class RegistrationSerializer(serializers.Serializer):
     @staticmethod
     def send_registration_email(email, code):
         message = EmailMessage(
-            subject="Social feed registration",
-            body=f"This is your registration code =>> {code}",
+            subject="Luna registration",  # backend is http://aquarius.propulsion-learn.ch/backend/api/
+            body=f"This is your registration link =>> http://aquarius.propulsion-learn.ch/registration/validation?code={code}&email={email}",
             to=[email],
             # here we put [] around email so that we could use
             # this for multiple user emails. Otherwise that would not work.
