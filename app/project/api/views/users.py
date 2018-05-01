d127e274042f4ec309608602cb2aa2483a6ee38a
Sirinya <30613818+halfpintutopia@users.noreply.github.com>
2018-05-01 17:24:54 +0200

    merge

--- app/project/api/views/users.py
+++ app/project/api/views/users.py
@@ -4,7 +4,7 @@ from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnl
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from project.api.serializers.users import UserSerializer, UserProfileSerializer
+from project.api.serializers.users import UserSerializer
 
 User = get_user_model()
 
