0ebf430164e76828292e2cd4e6d2ede3885c8ec9
Sirinya <30613818+halfpintutopia@users.noreply.github.com>
2018-05-01 14:30:39 +0200

    added search view and url

---
+++ app/project/api/views/search.py
@@ -0,0 +1,29 @@
+from django.db.models import Q
+from rest_framework.generics import ListAPIView
+from project.api.serializers.users import UserSerializer
+from project.restaurant.feed.models import Restaurant
+
+
+# @route   GET api/search
+# @desc    Get the review, restaurant or user profile
+# @access  Public
+class SearchListView(ListAPIView):
+    serializer_class = UserSerializer
+    queryset = Restaurant.objects.all()
+
+    def filter_queryset(self, queryset):
+        search_string = self.request.query_params.get('search')
+        if search_string:
+            queryset = queryset.filter(
+                Q(user__contains=search_string) |
+                Q(restaurant__contains=search_string) |
+                Q(first_name__contains=search_string) |
+                Q(last_name__contains=search_string) |
+                Q(review__contain=search_string)
+            )
+        return queryset
+
+
+# class UserGetView(RetrieveAPIView):
+#     serializer_class = UserSerializer
+#     queryset = User.objects.all()
