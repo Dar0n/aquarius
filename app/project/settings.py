a5d96dea1cb63a1a3bd4af5153db514d7fcd1b18
Ilya <shatuncheg@gmail.com>
2018-05-02 19:57:00 +0200

    Change static folder in the settings and nginx.conf to static-files, so they don't collapse with react static files.

--- app/project/settings.py
+++ app/project/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static-files/'
 STATIC_ROOT = '/static-files'
 
 MEDIA_URL = '/media/'
